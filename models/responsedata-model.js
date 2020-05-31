f9eeee6eeebdf202e7263ee582933cf65a21a591
yasodhakaturi <yasodha.bitra@gmail.com>
2020-05-30 22:44:59 -0700

    processedata-model

--- models/responsedata-model.js
+++ models/responsedata-model.js
@@ -1,23 +1,27 @@
 const mongoose = require('../common/services/mongoose.service').mongoose;
 const Schema = mongoose.Schema;
 
-const processeddataSchema = new Schema({
+const responsedataSchema = new Schema({
+    mobilenumber:{
+        type:Number,
+        required: [true, 'mobilenumber can not be empty!'],
+    },
+    responsedata:{
+type:String,
+required:[true,'response data cant be empty!']
+
+    },
     batchid:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'requesteddata'
     },
-    mobilenumber:{
-        type:number,
-        required: [true, 'mobilenumber can not be empty!']
-    },
-    webhook:{
-        type:String
-    },
+    
     status:{
         type:String,
         enum:['newrequest','inprogress','completed','submitted']
     },
-    jobid:{
-    type:Number
-}
-});
+    submittedate:{
+        type:Date
+        
+    }
+    });
