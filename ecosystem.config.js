696b362c15bd135c2df11b0b8a150a0163ab8e71
Sirish Bitra <sirish.bitra@gmail.com>
2020-06-26 11:10:09 -0700

    exosystem js updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps : [{
     "name": "mnp_lookup_app",
     "script": "./bin/www",
-    "node_args": ["--max_old_space_size=500"],
+    "node_args": ["--harmony", "--max_old_space_size=500"],
+    "instances": 1,
     "max_memory_restart": "480"
   }]
 };
